976c6062398c3a421dd8ed68b9180007c3a5d63a
ashishmohanty10 <ashishmohaty10@gmail.com>
2025-01-03 06:17:47 +0530

    feat: add test case for user metadata endpoint

--- tests/index.test.js
+++ tests/index.test.js
@@ -66,3 +66,81 @@ describe("Authentication", () => {
     expect(response.statusCode).toBe(403);
   });
 });
+
+describe("User info endpoints check", () => {
+  beforeAll(async () => {
+    let token = "";
+    let avatarId = "";
+
+    const username = `ashish-${Math.random()}`;
+    const password = "123456";
+
+    await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      username,
+      password,
+    });
+
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+      username,
+      password,
+    });
+
+    token = response.body.token;
+
+    const avatarResponse = await axios.post(
+      `${BACKEND_URL}/api/v1/admin/avatar`,
+      {
+        imageUrl:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm3RFDZM21teuCMFYx_AROjt-AzUwDBROFww&s",
+        name: "Timmy",
+      },
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    avatarId = avatarResponse.data.id;
+  });
+
+  test("User cant update their metadata with wrong avatar id", async () => {
+    const response = await axios.post(
+      `${BACKEND_URL}/api/v1/user/metadata`,
+      {
+        avatarId: "wrong-id",
+      },
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    expect(response.statusCode).toBe(400);
+  });
+
+  test("User can update their metadata with wrong avatar id", async () => {
+    const response = await axios.post(
+      `${BACKEND_URL}/api/v1/user/metadata`,
+      {
+        avatarId,
+      },
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("User should not able to update metadata if no auth header present", async () => {
+    const response = await axios.post(`${BACKEND_URL}/api/v1/user/metadata`, {
+      avatarId,
+    });
+
+    expect(response.statusCode).toBe(403);
+  });
+});
