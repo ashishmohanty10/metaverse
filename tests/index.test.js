f72999526e4403e1124befa80bbb170d484f62ca
ashishmohanty10 <ashishmohaty10@gmail.com>
2025-01-21 12:29:14 +0530

    test: refactor test code

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,42 @@
-const { default: axios } = require("axios");
+const axios2 = require("axios");
 
 const BACKEND_URL = "http://localhost:3000";
 const WS_URL = "ws://localhost:3001";
+
+const axios = {
+  post: async (...args) => {
+    try {
+      const res = await axios2.post(...args);
+      return res;
+    } catch (e) {
+      return e.response;
+    }
+  },
+  get: async (...args) => {
+    try {
+      const res = await axios2.get(...args);
+      return res;
+    } catch (e) {
+      return e.response;
+    }
+  },
+  put: async (...args) => {
+    try {
+      const res = await axios2.put(...args);
+      return res;
+    } catch (e) {
+      return e.response;
+    }
+  },
+  delete: async (...args) => {
+    try {
+      const res = await axios2.delete(...args);
+      return res;
+    } catch (e) {
+      return e.response;
+    }
+  },
+};
 
 describe("Authentication", () => {
   test("User should be able to signup only once", async () => {
